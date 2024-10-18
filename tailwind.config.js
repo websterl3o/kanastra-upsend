9b1ae7c19ae1cb7d342f1a75da4def88ff4de53e
Leonardo Webster <leonardowebster15@gmail.com>
2024-10-17 21:18:57 -0300

    Prototipo de tela inicial usando tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -16,5 +17,7 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        forms,
+    ],
 };
