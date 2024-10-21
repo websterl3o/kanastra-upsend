a734f97cf04755a6210704df4842d4b15de57b29
Leonardo Webster <leonardowebster15@gmail.com>
2024-10-20 21:53:47 -0300

    Corrigindo problema do npm run dev

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,11 @@ export default defineConfig({
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
